22f680e20f11551c6c6419cf608a72792267e53c
DH43EFd <pieterjan.de.muinck@ing.com>
2020-08-04 22:16:39 +0200

    provide event endpoint

--- api/src/setup/endpoints.js
+++ api/src/setup/endpoints.js
@@ -1,8 +1,17 @@
+const event = require('../sportsbook/event')
+
 module.exports = function(server) {
 
-    server.get('/', function(req, resp) {
+    server.get('/bookmakers/:book/events', async (req, resp) => {
+        await event.getEvents(req.params.book, req.query.sport)
+        .then(response => resp.send(response))
+        .catch((error) => {
+            resp.status(404).send(error.message)
+        })
+    }),
 
-        resp.send('Hello World!')
+    server.get('/bookmakers/:book/betoffers', async (req, resp) => {
+
     })
 
 } 
